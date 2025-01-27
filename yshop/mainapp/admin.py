3ffdd348e68162dd9a45a13e00148d187c7a6793
ananthakrishnanpta <ananthakrishnanpta@gmailcom>
2025-01-27 10:57:19 +0530

    everythin

--- yshop/mainapp/admin.py
+++ yshop/mainapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Product
 # Register your models here.
+
+
+admin.site.register(Product) # this registers the access of `Product` class model into admin panel
